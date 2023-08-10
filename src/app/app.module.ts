7e0773b7c8c249a5a6829e2afa322b656750621e
Fabiola Magalhaes <fabimagal99@gmail.com>
2023-08-10 11:36:36 -0300

    função calcula média de 3 valores. matcher toEqual

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { Componente2Component } from './componente2/componente2.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    Componente2Component
   ],
   imports: [
     BrowserModule,
